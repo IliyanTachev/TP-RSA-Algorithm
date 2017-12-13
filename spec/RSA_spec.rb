163b924de2fb89f383954fa568087acec2b30ee6
IliyanTachev <iliqn.tachev@gmail.com>
2017-12-13 02:27:41 +0200

    last

--- spec/RSA_spec.rb
+++ spec/RSA_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe RSA do
 	second_message = "EMINEM - Every Mom Is Nice Except Mine"
 
 	rsa = RSA.new(33,3,7)
-	rand_values = rsa.new_key
+	values = rsa.new_key
+	
+	random_values = RSA.new(values[0],values[1],values[2])
 
 	it "checks if n is right" do
 		expect(rsa.n).to eq(33)
@@ -21,13 +23,16 @@ RSpec.describe RSA do
         end
 
 	it "check if encrypt && decrypt work right" do
-		expect(rand_values.decrypt(rand_values.encrypt(first_message))).to eq "Welcome To the Hell!"
+		expect(rand_values.decrypt rand_values.encrypt first_message).to eq "Welcome To the Hell!"
 	end
 	
 	 it "check if encrypt && decrypt work right" do
-                expect(rand_values.decrypt(rand_values.encrypt(second_message))).to eq "EMINEM - Every Mom Is Nice Except Mine"
+                expect(rand_values.decrypt rand_values.encrypt second_message).to eq "EMINEM - Every Mom Is Nice Except Mine"
         end
-
+	
+	it "checks if decrypt && encrypt work eith random values" do
+		expect(random_values.decrypt random_values.encrypt "Welcome To the Hell!").to eq "Welcome To the Hell!"
+	end
 	
 	
 end
